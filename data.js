74f5cee211e2d65c611a27ea626453a5b7c232f6
pubggamerzytm <70851685+pubggamerzytm@users.noreply.github.com>
2021-08-22 10:55:24 +0530

    Update data.js

--- data.js
+++ data.js
@@ -1,9 +1,9 @@
 
 module.exports = {
-  token: '1286881333:AAE-DV_hsGGkpbhplVQHei3SoRNZK_DL8UA', // token fromm @BotFather
+  token: '1960673654:AAGn-c84pYh5UulNiRhJp90-q8AyMT1TPuw', // token fromm @BotFather
   mongoLink: 'mongodb+srv://User_1:arjuns1a2h3@cluster0.mfqk8.mongodb.net/aditya213?retryWrites=true&w=majority', // mongo-link from cloud.mongodb.com
-  dev: '1107921506', // developer`s id for sending him/her errors
-  chauname: '@pokidipo',//1st channel username
+  dev: '1237858645', // developer`s id for sending him/her errors
+  chauname: '@testnodejsbott',//1st channel username
   chauname2: '',//2nd channel username
   paycha: '',//payment channel username
   payme:'-1001354401420',//payment channel I'd starts with -100
